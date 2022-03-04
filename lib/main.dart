17ceea2b5ce00f8d30e55643b51d5cf2cf94eceb
Asifa Lestari <98302217+asifalestari@users.noreply.github.com>
2022-03-04 12:04:32 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:sampleproject/utils/text.dart';
+import 'package:sampleproject/widget/toprated.dart';
+import 'package:sampleproject/widget/trending.dart';
+import 'package:sampleproject/widget/tv.dart';
+import 'package:tmdb_api/tmdb_api.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.green),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List trendingmovies = [];
+  List topratedmovies = [];
+  List tv = [];
+  final String apikey = '67af5e631dcbb4d0981b06996fcd47bc';
+  final readaccesstoken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2FmNWU2MzFkY2JiNGQwOTgxYjA2OTk2ZmNkNDdiYyIsInN1YiI6IjYwNzQ1OTA0ZjkyNTMyMDAyOTFmZDczYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C_Bkz_T8OybTGo3HfYsESNjN46LBYdw3LHdF-1TzYYs';
+
+  @override
+  void initState() {
+    super.initState();
+    loadmovies();
+  }
+
+  loadmovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(ApiKeys(apikey, readaccesstoken),
+        logConfig: ConfigLogger(showLogs: true, showErrorLogs: true));
+    Map trendingresult = await tmdbWithCustomLogs.v3.trending.getTrending();
+    Map topratedresult = await tmdbWithCustomLogs.v3.movies.getTopRated();
+    Map tvresult = await tmdbWithCustomLogs.v3.tv.getPopular();
+    print(trendingresult);
+
+    setState(() {
+      trendingmovies = trendingresult['results'];
+      topratedmovies = topratedresult['results'];
+      tv = tvresult['results'];
+    });
+    print(trendingmovies);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: modified_text(
+          text: 'Flutter Movie App',
+          color: Colors.white,
+          size: 19,
+        ),
+      ),
+      body: ListView(children: [
+        TV(tv: tv),
+        TopRated(toprated: topratedmovies),
+        TrendingMovies(trending: trendingmovies)
+      ]),
+    );
+  }
+}
---
+++ lib/widget/trending.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:sampleproject/description.dart';
+import 'package:sampleproject/utils/text.dart';
+
+import 'trending.dart';
+
+class TrendingMovies extends StatelessWidget {
+  final List trending;
+
+  TrendingMovies({Key? key, required this.trending}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          modified_text(
+            text: "Trending Movies",
+            size: 26,
+            color: Colors.white,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+              height: 270,
+              child: ListView.builder(
+                  itemCount: trending.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Description(
+                                      name: trending[index]['title'],
+                                      bannerurl:
+                                          'https://image.tmdb.org/t/p/w500' +
+                                              trending[index]['backdrop_path'],
+                                      posterurl:
+                                          'https://image.tmdb.org/t/p/w500' +
+                                              trending[index]['backdrop_path'],
+                                      description: trending[index]['overview'],
+                                      vote: trending[index]['vote_average'],
+                                      launch_on: trending[index]['release_date']
+                                          .toString(),
+                                    )));
+                      },
+                      child: Container(
+                        width: 140,
+                        child: Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                      'https://image.tmdb.org/t/p/w500' +
+                                          trending[index]['poster_path']),
+                                ),
+                              ),
+                              height: 200,
+                            ),
+                            SizedBox(height: 5),
+                            Container(
+                              child: modified_text(
+                                size: 15,
+                                text: trending[index]['title'] != null
+                                    ? trending[index]['title']
+                                    : 'Loading',
+                                color: Color.fromARGB(255, 189, 119, 14),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }))
+        ],
+      ),
+    );
+  }
+}
